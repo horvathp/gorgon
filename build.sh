f476a4dc160ab53ee975e0bc8bcdf8b68fc10c8b
Peter Horvath <ejcspii@gmail.com>
2020-02-22 10:58:06 +0100

    modify build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
-mkdir -p atlgnd
-mkdir -p smogpgnd
+mkdir -p atlgnd_dir
+mkdir -p smogpgnd_dir
 
-mkdir -p atlgnd/download
-mkdir -p atlgnd/packets
-mkdir -p atlgnd/partial
-mkdir -p atlgnd/spectrum
-mkdir -p atlgnd/json
+mkdir -p atlgnd_dir/download
+mkdir -p atlgnd_dir/packets
+mkdir -p atlgnd_dir/partial
+mkdir -p atlgnd_dir/spectrum
+mkdir -p atlgnd_dir/json
 
-mkdir -p smogpgnd/download
-mkdir -p smogpgnd/packets
-mkdir -p smogpgnd/partial
-mkdir -p smogpgnd/spectrum
-mkdir -p smogpgnd/json
+mkdir -p smogpgnd_dir/download
+mkdir -p smogpgnd_dir/packets
+mkdir -p smogpgnd_dir/partial
+mkdir -p smogpgnd_dir/spectrum
+mkdir -p smogpgnd_dir/json
 
-gcc -D BUILD_SMOGP main.c lib/*.c ./ts_radecoder/*.c ./ao40/long/*.c ./ao40/short/*.c -o smogpgnd/smogpgnd -lm -lpthread -O3 -std=gnu90 #-Wall
-gcc -D BUILD_ATL1  main.c lib/*.c ./ts_radecoder/*.c ./ao40/long/*.c ./ao40/short/*.c -o atlgnd/atlgnd     -lm -lpthread -O3 -std=gnu90 #-Wall
-gcc packet_looter.c -o packet_looter -O3
-
-cp packet_looter atlgnd/
-mv packet_looter smogpgnd/
-
+cp build/smogpgnd smogpgnd_dir/
+cp build/atlgnd atlgnd_dir/
 
